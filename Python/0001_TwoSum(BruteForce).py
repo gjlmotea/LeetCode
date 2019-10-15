ac772a02500fcacb5603cc2b96448d178e832301
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-10-15 23:51:56 +0800

    Update 0001_TwoSum(BruteForce).py

--- Python/0001_TwoSum(BruteForce).py
+++ Python/0001_TwoSum(BruteForce).py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] + nums[j] == target:
                     return j, i
-        
 
-## 測試
-## nums = [2,7,11,15]
-## target = 9
 
-## print(Solution.twoSum(None, nums, target))
+#測試
+#nums = [2,7,11,15]
+#target = 9
+
+#print(Solution.twoSum(object(), nums, target))
