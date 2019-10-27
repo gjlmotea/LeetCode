95a6f158a12ac2508dd43004e7a27afe962a2be7
GJLMoTea <40420568+gjlmotea@users.noreply.github.com>
2019-10-27 12:33:19 +0800

    Update 0002_Add Two Numbers_v2.py

--- Python/0002_Add Two Numbers_v2.py
+++ Python/0002_Add Two Numbers_v2.py
@@ -5,27 +5,38 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3 = ListNode(0)
+        
         
         print(l1)
         print(l2)
+        carry = 0
+        l3 = ListNode
+        head = l3
         
         while True:
             sum = 0
-            
-            if l1 == None and l2 == None:
+            if l1 == None and l2 == None and carry == 0:
                 break
+                
+            sum = sum + carry
+            
             if l1 != None:
                 sum = sum + l1.val
                 l1 = l1.next
-            else:
-                sum = sum + l1.val
+            
                 
             if l2 != None:
                 sum = sum + l2.val
                 l2 = l2.next
+            
+            
+            if(sum > 9):
+                carry = 1
+                sum = sum % 10
             else:
-                sum = sum + l2.val
-                
-                
-        return l3
+                carry = 0
+            
+            l3.next = ListNode(sum)
+            l3 = l3.next
+            
+        return head.next
